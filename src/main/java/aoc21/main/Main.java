6e0d924027de95076e5ba6a3ff8e7dcf0488a3d3
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2021-12-13 10:31:10 +0100

    Solve Day13

--- src/main/java/aoc21/main/Main.java
+++ src/main/java/aoc21/main/Main.java
@@ -2,11 +2,9 @@ package aoc21.main;
 
 import aoc.days.Day;
 import aoc21.days.day1.Day1;
-<<<<<<< HEAD
 import aoc21.days.day10.Day10;
-=======
 import aoc21.days.day11.Day11;
->>>>>>> b90bafc1038c1d3bc1f9779123e3e53df2113488
+import aoc21.days.day13.Day13;
 import aoc21.days.day2.Day2;
 import aoc21.days.day3.Day3;
 import aoc21.days.day4.Day4;
@@ -19,13 +17,13 @@ import aoc21.days.day9.Day9;
 public class Main {
     private static final Day[] days =
             {new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(), new Day8(), new Day9()
-            ,new Day10(), new Day11()};
+            ,new Day10(), new Day11(), null, new Day13()};
 
     private static void runDay(int dayNumber) {
         days[dayNumber - 1].runAndPrint("input" + dayNumber);
     }
 
     public static void main(String[] args) {
-        runDay(11);
+        runDay(13);
     }
 }
